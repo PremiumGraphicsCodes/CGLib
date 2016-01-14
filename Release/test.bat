629c9721bc04a81c7ad3ed85d2d2ee6b2f75b12a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-01-14 22:09:52 +0900

    テスト結果集計用バッチを追加．

---
+++ Release/test.bat
@@ -0,0 +1,5 @@
+MathTest.exe --gtest_output=xml:math_test_result.xml
+PhysicsTest.exe --gtest_output=xml:physics_test_result.xml
+GraphicsTest.exe --gtest_output=xml:graphics_test_result.xml
+IOTest.exe --gtest_output=xml:io_test_result.xml
+ShaderTest.exe --gtest_output=xml:shader_test_result.xml
