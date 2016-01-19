0ad71c173690e1ca18b8a64faa859dd04f1bd16d
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-01-19 16:22:49 +0900

    完全なバイナリ保存に変更．

--- IO/CGVFile.cpp
+++ IO/CGVFile.cpp
@@ -7,6 +7,9 @@ using namespace Crystal::IO;
 
 bool CGVFile::save(const std::string& filename, const Volume3d<float, float>& volume)
 {
+	const auto& grid = volume.getGrid();
+	const auto& space = volume.getSpace();
+
 	std::ofstream stream;
 	stream.open(filename, std::ios::out | std::ios::binary | std::ios::trunc);
 
@@ -14,28 +17,29 @@ bool CGVFile::save(const std::string& filename, const Volume3d<float, float>& vo
 		return false;
 	}
 
-	const auto& grid = volume.getGrid();
-	const auto& space = volume.getSpace();
+	const std::string str("CGStudio File Format Version = 0.3.0");
+	stream.write(str.data(), 50);
 
-	stream << "CGStudioFileFormatVersion 0 3 0" << std::endl;
-	stream
-		<< "SpaceMin" << " "
-		<< space.getStart().getX() << " "
-		<< space.getStart().getY() << " " 
-		<< space.getStart().getZ()
-		<< std::endl;
-	stream
-		<< "SpaceLength" << " "
-		<< space.getLengths().getX() << " "
-		<< space.getLengths().getY() << " "
-		<< space.getLengths().getZ()
-		<< std::endl;
-	stream
-		<< "SpaceResolution" << " "
-		<< grid.getSizeX() << " "
-		<< grid.getSizeY() << " "
-		<< grid.getSizeZ()
-		<< std::endl;
+	const auto startx = space.getStart().getX();
+	stream.write((char*)&startx, sizeof(startx));
+	const auto starty = space.getStart().getY();
+	stream.write((char*)&starty, sizeof(starty));
+	const auto startz = space.getStart().getY();
+	stream.write((char*)&startz, sizeof(startz));
+
+	const auto lengthx = space.getLengths().getX();
+	stream.write((char*)&lengthx, sizeof(lengthx));
+	const auto lengthy = space.getLengths().getY();
+	stream.write((char*)&lengthy, sizeof(lengthy));
+	const auto lengthz = space.getLengths().getZ();
+	stream.write((char*)&lengthz, sizeof(lengthz));
+
+	const auto resx = grid.getSizeX();
+	stream.write((char*)&resx, sizeof(resx));
+	const auto resy = grid.getSizeY();
+	stream.write((char*)&resy, sizeof(resy));
+	const auto resz = grid.getSizeZ();
+	stream.write((char*)&resz, sizeof(resz));
 
 	for (int i = 0; i< grid.getSizeX(); i++) {
 		for (int j = 0; j < grid.getSizeY(); ++j) {
@@ -60,58 +64,34 @@ bool CGVFile::load(const std::string& filename)
 		return false;
 	}
 
-	std::string str;
-	stream >> str;
+	char str[50];
+	stream.read(str, 50);
 
-	assert(str == "CGStudioFileFormatVersion");
+	const float startx = 0.0f;
+	stream.read((char*)&startx, sizeof(startx));
+	const float starty = 0.0f;
+	stream.read((char*)&starty, sizeof(starty));
+	const float startz = 0.0f;
+	stream.read((char*)&startz, sizeof(startz));
 
-	int majorversion = 0;
-	stream >> majorversion;
-	int minorversion = 0;
-	stream >> minorversion;
-	int patchversion = 0;
-	stream >> patchversion;
-
-	stream >> str;
-	assert(str == "SpaceMin");
-
-	float minx = 0;
-	stream >> minx;
-
-	float miny = 0;
-	stream >> miny;
-
-	float minz = 0;
-	stream >> minz;
-
-	stream >> str;
-	assert(str == "SpaceLength");
-
-	float lengthx = 0;
-	stream >> lengthx;
-
-	float lengthy = 0;
-	stream >> lengthy;
-
-	float lengthz = 0;
-	stream >> lengthz;
-
-	stream >> str;
-	assert(str == "SpaceResolution");
+	float lengthx = 0.0f;
+	stream.read((char*)&lengthx, sizeof(lengthx));
+	float lengthy = 0.0f;
+	stream.read((char*)&lengthy, sizeof(lengthy));
+	float lengthz = 0.0f;
+	stream.read((char*)&lengthz, sizeof(lengthz));
 
 	int resx = 0;
-	stream >> resx;
+	stream.read((char*)&resx, sizeof(resx));
 
 	int resy = 0;
-	stream >> resy;
+	stream.read((char*)&resy, sizeof(resy));
 
 	int resz = 0;
-	stream >> resz;
+	stream.read((char*)&resz, sizeof(resz));
 
-	stream >> str;
-
+	auto space = Space3d<float>(Vector3d<float>(startx, starty, startz), Vector3d<float>(lengthx, lengthy, lengthz));
 	auto grid = Grid3d<float>(resx, resy, resz);
-	auto space = Space3d<float>(Vector3d<float>(minx, miny, minz), Vector3d<float>(lengthx, lengthy, lengthz));
 
 	for (int i = 0; i< resx; i++) {
 		for (int j = 0; j < resy; ++j) {
@@ -122,7 +102,6 @@ bool CGVFile::load(const std::string& filename)
 			}
 		}
 	}
-
 	this->volume = Volume3d<float, float>(space, grid);
 
 	return true;
