90455d1d259f1cc986cad496193df05a1d5405dc
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-01-21 18:50:58 +0900

    コミット漏れ．

---
+++ Graphics/Image.cpp
@@ -0,0 +1,68 @@
+#include "Image.h"
+#pragma warning(disable:4996)
+
+#include "stb_image.h"
+
+using namespace Crystal::Graphics;
+
+template<typename T>
+void Image<T>::read(const std::string& filename)
+{
+	int width = 0;
+	int height = 0;
+	int comp = 0;
+	const auto loaded = stb::stbi_load(filename.c_str(), &width, &height, &comp, 4);
+	assert(loaded != nullptr);
+	assert(comp == stb::STBI_rgb || comp == stb::STBI_rgb_alpha);
+	std::vector<unsigned char> values(width * height * 4);
+	//if (comp == stb::STBI_rgb_alpha) {
+	for (int i = 0; i < width * height; ++i) {
+		values[i * 4] = loaded[i * 4];
+		values[i * 4 + 1] = loaded[i * 4 + 1];
+		values[i * 4 + 2] = loaded[i * 4 + 2];
+		values[i * 4 + 3] = loaded[i * 4 + 3];
+	}/*
+	else {
+		int next = 0;
+		for (int i = 0; i < width * height; ++i) {
+			values[i * 4] = loaded[next++];
+			values[i * 4 + 1] = loaded[next++];
+			values[i * 4 + 2] = loaded[next++];
+			values[i * 4 + 3] = 255;
+		}
+	}
+	*/
+	this->width = width;
+	this->height = height;
+	this->values =values;
+}
+
+template<typename T>
+void Image<T>::save(const std::string& filename, const ImageFileFormat format) const {
+	std::vector<T> data = values;
+	int comp = 4;
+	if (format == ImageFileFormat::PNG) {
+		stb::stbi_write_png(filename.c_str(), width, height, comp, data.data(), width * comp);
+	}
+	else if (format == ImageFileFormat::BMP) {
+		stb::stbi_write_bmp(filename.c_str(), width, height, comp, data.data());
+	}
+	else {
+		assert(false);
+	}
+}
+
+template<typename T>
+void Image<T>::changeSize(const int sizeX, const int sizeY)
+{
+	std::vector<T> dest(sizeX * sizeY * 4);
+	stb::stbir_resize_uint8(values.data(), width, height, 0, dest.data(), sizeX, sizeY, 0, 4);
+
+	this->width = sizeX;
+	this->height = sizeY;
+	this->values = dest;
+}
+
+//template class ImageRGBA<float>;
+//template class ImageRGBA<double>;
+template class Image<unsigned char>;
---
+++ GraphicsTest/ImageTest.cpp
@@ -0,0 +1,81 @@
+#include "gtest/gtest.h"
+ 
+#include "../Graphics/Image.h"
+
+using namespace Crystal::Graphics;
+
+TEST( ImageRGBATest, ContructTest )
+{
+	Image<float> image;
+	EXPECT_EQ( 0, image.getWidth() );
+	EXPECT_EQ( 0, image.getHeight() );
+	EXPECT_TRUE( image.getValues().empty() );
+}
+
+TEST( ImageRGBATest, TestGetColor )
+{
+	Image<float> color( 2, 1);
+	color.setColor(0, 0, ColorRGBA<float>::Red() );
+	EXPECT_EQ( ColorRGBA<float>::Red(), color.getColor(0, 0) );
+	EXPECT_EQ( ColorRGBA<float>(0,0,0,0), color.getColor(1, 0) );
+
+}
+
+TEST(ImageRGBATest, TestColors)
+{
+	EXPECT_EQ(ColorRGBA<float>::Black(), Image<float>::Black(1, 1).getColor(0, 0));
+	EXPECT_EQ(ColorRGBA<float>::White(), Image<float>::White(1, 1).getColor(0, 0));
+
+}
+
+TEST(ImageRGBATest, TestWritePNG)
+{
+	auto image = Image<unsigned char>::Red(32, 32);
+	//buffer.setColor(1, 0, ColorRGBA<unsigned char>(255, 0, 0, 0));
+	image.save("../TestFile/IO/Image/testWrite.png", ImageFileFormat::PNG );
+	const Image<unsigned char> actual("../TestFile/IO/Image/testExpected.png");
+	EXPECT_EQ(ColorRGBA<unsigned char>(255,0,0,255), actual.getColor(0, 0));
+	EXPECT_EQ(ColorRGBA<unsigned char>(255, 0, 0, 255), actual.getColor(31, 31));
+
+	EXPECT_EQ(32, actual.getWidth());
+	EXPECT_EQ(32, actual.getHeight());
+
+	EXPECT_EQ(image, actual);
+	//stbi_save("test.png");
+}
+
+TEST(ImageRGBATest, TestWriteBMP)
+{
+	auto image = Image<unsigned char>::Red(32, 32);
+	//buffer.setColor(1, 0, ColorRGBA<unsigned char>(255, 0, 0, 0));
+	image.save("../TestFile/IO/Image/actual.bmp", ImageFileFormat::BMP);
+	auto actual = Image<unsigned char>::Red(32, 32);
+	actual.read("../TestFile/IO/Image/expected.bmp");
+	EXPECT_EQ(ColorRGBA<unsigned char>(255, 0, 0, 255), actual.getColor(0, 0));
+	EXPECT_EQ(ColorRGBA<unsigned char>(255, 0, 0, 255), actual.getColor(31, 31));
+
+	EXPECT_EQ(32, actual.getWidth());
+	EXPECT_EQ(32, actual.getHeight());
+
+	EXPECT_EQ(image, actual);
+	//stbi_save("test.png");
+}
+
+
+TEST(ImageRGBATest, TestChangeSize)
+{
+	auto image = Image<unsigned char>::Red(12, 12);
+	image.changeSize(24, 24);
+	image.save("../TestFile/Graphics/ChangeSize.png", ImageFileFormat::PNG );
+}
+
+/*
+TYPED_TEST(ColorRGBATest, TestScale)
+{
+	const auto filename = "../TestFile/UI/2x2.png";
+	ImageRGBA<unsigned char> image;
+	image.read(filename);
+	image.save("../TestFile/UI/2x2Result.png");
+	//src.createScaled(scale);
+}
+*/
