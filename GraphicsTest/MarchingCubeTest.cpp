9dc0d36356e39c89c4dbc821809ad7386fdae70f
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-01-27 19:30:45 +0900

    改良MCのテストコードを追加．

---
+++ GraphicsTest/MarchingCubeTest.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+
+#include "../Graphics/MarchingCube.h"
+#include "../Math/Volume3d.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Graphics::Experiment;
+
+
+TEST(MarchingCubeTest, TestMarchScalarSpace)
+{
+	MarchingCube mc;
+
+	Space3d<float> s(Vector3d<float> (0, 0, 0), Vector3d<float>(10, 10, 10));
+	Grid3d<float> grid(4, 4, 4);
+	grid.set(0, 0, 0, 0.8);
+	grid.set(1, 0, 0, 0.8);
+	Volume3d<float, float> ss(s, grid);
+
+	mc.march(ss, 0.5);
+}
