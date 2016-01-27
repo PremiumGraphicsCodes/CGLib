e0f80376b31e5a9d4317471eb1ac5b1d66765b6a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-01-27 14:00:41 +0900

    テストケースを補強．

--- GraphicsTest/PolygonReductionTest.cpp
+++ GraphicsTest/PolygonReductionTest.cpp
@@ -5,17 +5,38 @@ using namespace Crystal::Math;
 using namespace Crystal::Graphics::Experiment;
 
 
-TEST(PolygonReductionTest, TestCreateVertex)
+TEST(PolygonReductionTest, TestRemoveOverlappedVertices)
 {
 	TriangleMesh mesh;
 	const auto v1 = mesh.createVertex( Vector3d<float>(0, 0, 0) );
 	const auto v2 = mesh.createVertex( Vector3d<float>(1, 0, 0) );
-	//const auto v3 = mesh.createVertex( Vector3d<float>(0, 1, 0) );
-	const auto v4 = mesh.createVertex(Vector3d<float>(0, 0, 0));
+	const auto v3 = mesh.createVertex( Vector3d<float>(0, 1, 0) );
+	const auto v4 = mesh.createVertex( Vector3d<float>(-1, 0, 0) );
+	const auto v5 = mesh.createVertex( Vector3d<float>(0, 0, 0) );
+	const auto v6 = mesh.createVertex( Vector3d<float>(0, 1, 0) );
 
-	//mesh.createFace({ v1, v2, v3 });
-	//mesh.createFace()
 	mesh.removeOverlappedVerticies();
 
-	EXPECT_EQ(2, mesh.getVertices().size());
+	EXPECT_EQ(4, mesh.getVertices().size());
+}
+
+TEST(PolygonReductionTest, TestCreateFacesVertices)
+{
+	TriangleMesh mesh;
+	const auto v1 = mesh.createVertex(Vector3d<float>(0, 0, 0));
+	const auto v2 = mesh.createVertex(Vector3d<float>(1, 0, 0));
+	const auto v3 = mesh.createVertex(Vector3d<float>(0, 1, 0));
+	const auto v4 = mesh.createVertex(Vector3d<float>(-1, 0, 0));
+	const auto v5 = mesh.createVertex(Vector3d<float>(0, 0, 0));
+	const auto v6 = mesh.createVertex(Vector3d<float>(0, 1, 0));
+
+	auto f1 = mesh.createFace({ v1, v2, v3 });
+	auto f2 = mesh.createFace({ v4, v5, v6 });
+
+	EXPECT_TRUE(f1->hasVertex(v1));
+	EXPECT_FALSE(f1->hasVertex(v5));
+	f1->replaceVertex(v1, v5);
+
+	EXPECT_FALSE(f1->hasVertex(v1));
+	EXPECT_TRUE(f1->hasVertex(v5));
 }
