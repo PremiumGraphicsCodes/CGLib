2010d732933f818366bc4ec6bd089f3f8517b0c6
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-02-29 11:43:21 +0900

    リファクタリング．

--- Physics/SPHKernel.cpp
+++ Physics/SPHKernel.cpp
@@ -1,3 +1,44 @@
 #include "SPHKernel.h"
 
+using namespace Crystal::Math;
 using namespace Crystal::Physics;
+
+template<typename GeomType>
+float SPHKernel<GeomType>::getPoly6Kernel(const GeomType distance, const GeomType effectLength) {
+	const auto poly6Constant = 315.0f / (64.0f * Math::Tolerance<GeomType>::getPI() * pow(effectLength, 9));
+	return poly6Constant * pow(effectLength * effectLength - distance * distance, 3);
+}
+
+template<typename GeomType>
+Vector3d<GeomType> SPHKernel<GeomType>::getPoly6KernelGradient(const Vector3d<GeomType>& distanceVector, const GeomType effectLength) {
+	const auto distance = distanceVector.getLength();
+	const auto poly6ConstantGradient = 945.0f / (32.0f * Tolerance<GeomType>::getPI() * pow(effectLength, 9));
+	const auto factor = poly6ConstantGradient * pow(effectLength * effectLength - distance * distance, 2);
+	return distanceVector * factor;
+}
+
+template<typename GeomType>
+float SPHKernel<GeomType>::getPoly6KernelLaplacian(const GeomType distance, const GeomType effectLength)
+{
+	const auto poly6ConstantLaplacian = 945.0f / (32.0f * Tolerance<GeomType>::getPI() * pow(effectLength, 9));
+	return poly6ConstantLaplacian * (effectLength * effectLength - distance * distance)
+		* (42.0f * distance * distance - 18.0f * effectLength * effectLength);
+}
+
+template<typename GeomType>
+Vector3d<GeomType> SPHKernel<GeomType>::getSpikyKernelGradient(const Vector3d<GeomType> &distanceVector, const float effectLength)
+{
+	const auto constant = 45.0f / (Math::Tolerance<GeomType>::getPI() * pow(effectLength, 6));
+	const auto distance = distanceVector.getLength();
+	return distanceVector * constant * pow(effectLength - distance, 2) / distance;
+}
+
+template<typename GeomType>
+float SPHKernel<GeomType>::getViscosityKernelLaplacian(const GeomType distance, const GeomType effectLength)
+{
+	const auto constant = 45.0f / (Math::Tolerance<GeomType>::getPI() * pow(effectLength, 6));
+	return (effectLength - distance) * constant;
+}
+
+
+template class SPHKernel<float>;
--- Physics/SPHKernel.h
+++ Physics/SPHKernel.h
@@ -10,35 +10,15 @@ template<typename GeomType>
 class SPHKernel
 {
 public:
-	float getPoly6Kernel(const GeomType distance, const GeomType effectLength) {
-		const auto poly6Constant = 315.0f / (64.0f * Math::Tolerance<GeomType>::getPI() * pow(effectLength, 9));
-		return poly6Constant * pow(effectLength * effectLength - distance * distance, 3);
-	}
+	float getPoly6Kernel(const GeomType distance, const GeomType effectLength);
 
-	Math::Vector3d<GeomType> getPoly6KernelGradient(const Math::Vector3d<GeomType>& distanceVector, const GeomType effectLength) {
-		const auto distance = distanceVector.getLength();
-		const auto poly6ConstantGradient = 945.0f / (32.0f * Tolerance<GeomType>::getPI() * pow(effectLength, 9));
-		const auto factor = poly6ConstantGradient * pow(effectLength * effectLength - distance * distance, 2);
-		return distanceVector * factor;
-	}
+	Math::Vector3d<GeomType> getPoly6KernelGradient(const Math::Vector3d<GeomType>& distanceVector, const GeomType effectLength);
 
-	float getPoly6KernelLaplacian(const GeomType distance, const GeomType effectLength) {
-		const auto poly6ConstantLaplacian = 945.0f / (32.0f * Tolerance<GeomType>::getPI() * pow(effectLength, 9));
-		return poly6ConstantLaplacian * (effectLength * effectLength - distance * distance)
-			* (42.0f * distance * distance - 18.0f * effectLength * effectLength);
-	}
+	float getPoly6KernelLaplacian(const GeomType distance, const GeomType effectLength);
 
-	Math::Vector3d<GeomType> getSpikyKernelGradient(const Math::Vector3d<GeomType> &distanceVector, const float effectLength) {
-		const auto constant = 45.0f / (Math::Tolerance<GeomType>::getPI() * pow(effectLength, 6));
-		const auto distance = distanceVector.getLength();
-		return distanceVector * constant * pow(effectLength - distance, 2) / distance;
-	}
+	Math::Vector3d<GeomType> getSpikyKernelGradient(const Math::Vector3d<GeomType> &distanceVector, const float effectLength);
 
-	float getViscosityKernelLaplacian(const GeomType distance, const GeomType effectLength) {
-		const auto constant = 45.0f / (Math::Tolerance<GeomType>::getPI() * pow(effectLength, 6));
-		return (effectLength - distance) * constant;
-	}
-
+	float getViscosityKernelLaplacian(const GeomType distance, const GeomType effectLength);
 };
 	}
 }
