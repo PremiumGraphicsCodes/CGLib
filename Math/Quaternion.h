c6e8510b08160a3cec486f41367e128934579274
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-03-15 19:34:51 +0900

    クォータニオンのcppを作成．

--- Math/Quaternion.h
+++ Math/Quaternion.h
@@ -11,71 +11,21 @@ template<typename T>
 class Quaternion
 {
 public:	
-	Quaternion(void) :
-		x(0.0), y(0.0), z(0.0), w(1.0)
-	{
-	}
+	Quaternion(void);
 
-	Quaternion(const T x, const T y, const T z, const T w) :
-		x(x), y(y), z(z), w(w)
-	{
-	}
-	
-	Quaternion(const Vector3d<T>& axis, const T angle) :
-		x(axis.getX() * sin(angle * 0.5f)),
-		y(axis.getY() * sin(angle * 0.5f)),
-		z(axis.getZ() * sin(angle * 0.5f)),
-		w(cos(angle * 0.5f))
-	{
-		assert(axis.isNormalized());
-	}
+	Quaternion(const T x, const T y, const T z, const T w);
 
+	Quaternion(const Vector3d<T>& axis, const T angle);
 
-	Matrix3d<T> toMatrix() const {
-		const auto x00 = (1.0f - 2.0f * y * y - 2.0f * z * z);
-		const auto x01 = (2.0f * x * y - 2.0f * z * w);
-		const auto x02 = (2.0f * x * z + 2.0f * y * w);
+	Matrix3d<T> toMatrix() const;
 
-		const auto x10 = (2.0f * x * y + 2.0f * z * w);
-		const auto x11 = (1.0f - 2.0f * x * x - 2.0f * z * z);
-		const auto x12 = (2.0f * y * z - 2.0f * x * w);
+	Quaternion& operator+=(const Quaternion& rhs);
 
-		const auto x20 = (2.0f * x * z - 2.0f * y * w);
-		const auto x21 = (2.0f * y * z + 2.0f * x * w);
-		const auto x22 = (1.0f - 2.0f * x * x - 2.0f * y * y);
+	T getNorm() const;
 
-		return Matrix3d<T>(
-			x00, x01, x02,
-			x10, x11, x12,
-			x20, x21, x22
-			);
-	}
-	
-	Quaternion& operator+=(const Quaternion& rhs) {
-		x += rhs.x;
-		y += rhs.y;
-		z += rhs.z;
-		w += rhs.w;
-		return *(this);
-	}
+	Quaternion& normalize();
 
-	T getNorm() const {
-		return sqrt( pow(x, 2) + pow(y, 2) + pow(z, 2) + pow(w, 2) );
-	}
-
-	Quaternion& normalize() {
-		const T norm = getNorm();
-		x /= norm;
-		y /= norm;
-		z /= norm;
-		z /= norm;
-		assert(isNormalized());
-		return *this;
-	}
-
-	bool isNormalized() const {
-		return Tolerancef::isEqualStrictly(getNorm(), 1.0);
-	}
+	bool isNormalized() const;
 
 	T getX() const { return x; }
 
