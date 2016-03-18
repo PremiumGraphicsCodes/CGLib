e9d3978a10e216ca32c61550991a2d9e4849b21b
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-03-18 21:46:33 +0900

    デモケースを調整．

--- Sample/BulletRigidSample.cpp
+++ Sample/BulletRigidSample.cpp
@@ -23,28 +23,25 @@ void BulletRigidSample::setup()
 {
 	{
 		rigid1 = std::make_unique<BulletRigid>(Vector3d<float>(0.5f, 0.5f, 0.5f), Vector3d<float>(0.0f, -2.0f, 0.0f), 10.0f);
-		//rigid1->getBody()->applyForce(btVector3(100.0f, 0.0f, 0.0f), btVector3(0.5f, 0.0f, 0.0f));
-		rigid1->getBody()->setAngularVelocity(btVector3(1.0f, 0.0f, 0.0f));
+		rigid1->getBody()->applyForce(btVector3(2000.0f, 0.0f, 0.0f), btVector3(0.5f, 0.0f, 0.0f));
+		//rigid1->getBody()->setAngularVelocity(btVector3(1.0f, 0.0f, 0.0f));
 		world.add(rigid1.get());
 	}
 	{
-		rigid2 = std::make_unique<BulletRigid>(Vector3d<float>(0.5f, 5.0f, 0.5f), Vector3d<float>(2.0f, 0.0f, 0.0f), 0.1f);
+		rigid2 = std::make_unique<BulletRigid>(Vector3d<float>(0.5f, 2.0f, 0.5f), Vector3d<float>(2.0f, 0.0f, 0.0f), 0.1f);
 		world.add(rigid2.get());
 	}
-	/*
 	{
-		const Box<float> box(Vector3d<float>(0.9f, 4.0f, 0.0f), Vector3d<float>(1.9f, 5.0f, 1.0f));
-		rigid3 = std::make_unique<BulletRigid>(box, 1.0f);
+		rigid3 = std::make_unique<BulletRigid>(Vector3d<float>(0.5f, 2.0f, 0.5f), Vector3d<float>(2.0f, 10.0f, 0.0f), 0.1f);
 		world.add(rigid3.get());
 	}
-	*/
 
 
 	{
 		ground = std::make_unique<BulletRigid>(Vector3d<float>(50.0f, 5.0f, 50.0f), Vector3d<float>(0.0f, -10.0f, 0.0f), 0.0f);
 		world.add(ground.get());
 	}
-	world.setExternalForce(Vector3d<float>(0, 0, 0));
+	world.setExternalForce(Vector3d<float>(0, -9.8, 0));
 
 }
 
@@ -71,7 +68,6 @@ void BulletRigidSample::demonstrate()
 			Crystal::Graphics::Point pt(p, ColorRGBA<float>(1, 0, 0, 1), 10.0f);
 			buffer.add(pt);
 		}
-
 	}
 	{
 		const auto& surfels = rigid2->toSurlfes(0.25f).toPositions();
@@ -80,6 +76,14 @@ void BulletRigidSample::demonstrate()
 			buffer.add(pt);
 		}
 	}
+	{
+		const auto& surfels = rigid3->toSurlfes(0.25f).toPositions();
+		for (const auto& p : surfels) {
+			Crystal::Graphics::Point pt(p, ColorRGBA<float>(1, 0, 0, 1), 10.0f);
+			buffer.add(pt);
+		}
+	}
+
 	renderer.render(camera, buffer);
 
 	/*
