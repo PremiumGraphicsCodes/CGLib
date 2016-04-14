58cab7981b37457e436d930d6188a81b424f90c4
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-04-14 20:37:12 +0900

    テクスチャを線形補間ベースに変更．

--- Shader/Texture.cpp
+++ Shader/Texture.cpp
@@ -21,8 +21,8 @@ bool Texture<ColorType>::create(const Image<ColorType>& image, const int id)
 	glBindTexture(GL_TEXTURE_2D, texHandle);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getValues().data());
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
