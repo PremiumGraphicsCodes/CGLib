5fabd067174defd5fe4cbfa2bf8845e86aa4cfe1
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-04-19 10:34:22 +0900

    DepthBufferを追加．

--- Shader/Texture.cpp
+++ Shader/Texture.cpp
@@ -90,4 +90,44 @@ void Texturef::unbind() const
 }
 
 
+DepthTexture::DepthTexture(const Imagef& image, const int id)
+{
+	create(image, id);
+}
+
+bool DepthTexture::create(const Imagef& image,const int id)
+{
+	this->id = id;
+	this->width = image.getWidth();
+	this->height = image.getHeight();
+
+	glActiveTexture(GL_TEXTURE0 + id);
+	glGenTextures(1, &texHandle);
+	glBindTexture(GL_TEXTURE_2D, texHandle);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, image.getWidth(), image.getHeight(), 0, GL_DEPTH_COMPONENT, GL_FLOAT, image.getValues().data());
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	return (GL_NO_ERROR == glGetError());
+}
+
+void DepthTexture::bind() const
+{
+	glActiveTexture(GL_TEXTURE0 + id);
+	glBindTexture(GL_TEXTURE_2D, texHandle);
+}
+
+void DepthTexture::unbind() const
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glActiveTexture(GL_TEXTURE0);
+}
+
+
 template class Texture<unsigned char>;
--- Shader/Texture.h
+++ Shader/Texture.h
@@ -72,6 +72,38 @@ private:
 	int height;
 };
 
+class DepthTexture
+{
+public:
+	DepthTexture(const int id = 0) :
+		id(id)
+	{}
+
+	DepthTexture(const Graphics::Imagef& image, const int id = 0);
+
+	bool create(const Graphics::Imagef& image, const int id = 0);
+
+	~DepthTexture() { unbind(); }
+
+	void bind() const;
+
+	void unbind() const;
+
+	int getId() const { return id; }
+
+	GLuint getTexHandle() const { return texHandle; }
+
+	int getWidth() const { return width; }
+
+	int getHeight() const { return height; }
+
+private:
+	int id;
+	GLuint texHandle;
+	int width;
+	int height;
+
+};
 	}
 }
 
