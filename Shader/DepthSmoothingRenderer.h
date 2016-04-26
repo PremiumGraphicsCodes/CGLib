fd4f97cdfefe056a59e1b0101875c7aa576dc4dc
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-04-26 15:43:26 +0900

    Depth専用Smoothingを追加．

---
+++ Shader/DepthSmoothingRenderer.h
@@ -0,0 +1,37 @@
+#ifndef __CRYSTAL_SHADER_DEPTH_SMOOTHING_RENDERER_H__
+#define __CRYSTAL_SHADER_DEPTH_SMOOTHING_RENDERER_H__
+
+#include "../Graphics/ICamera.h"
+#include "../Graphics/PointBuffer.h"
+#include "Texture.h"
+#include "DepthTexture.h"
+#include "IRenderer.h"
+
+namespace Crystal {
+	namespace Shader {
+
+class DepthSmoothingRenderer
+{
+public:
+	void render(const ITexture& texture, bool isX);
+
+	bool build();
+
+private:
+	std::string getBuildinVertexShaderSource();
+
+	std::string getBuildinFragmentShaderSource();
+
+	void findLocation();
+
+
+private:
+	ShaderObject shader;
+	ShaderUnit vertexShader;
+	ShaderUnit fragmentShader;
+};
+
+	}
+}
+
+#endif
