a077dec5552fed8bc07a540b8e797f0c1fddfb2d
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-06-03 13:07:52 +0900

    テストコードを補強．

--- MathTest/SPHKernelTest.cpp
+++ MathTest/SPHKernelTest.cpp
@@ -27,16 +27,32 @@ TEST(SPHKernelTest, TestGetCubicSpiline)
 TEST(SPHKernelTest, TestGetCubicSpilineByVector)
 {
 	SPHKernel<float> kernel;
-	const auto actual = kernel.getCubicSpline(Vector3d<float>(1, 0, 0), 1.0f);
-	const auto expected = 1.0f / (4 * Tolerance<float>::getPI());
-	EXPECT_TRUE(Tolerance<float>::isEqualLoosely(expected, actual));
+	{
+		const auto actual = kernel.getCubicSpline(Vector3d<float>(1, 0, 0), 1.0f);
+		const auto expected = 1.0f / (4 * Tolerance<float>::getPI());
+		EXPECT_TRUE(Tolerance<float>::isEqualLoosely(expected, actual));
+	}
+	{
+		const auto actual = kernel.getCubicSpline(Vector3d<float>(2, 0, 0), 2.0f);
+		const auto expected = 1.0f / (4 * Tolerance<float>::getPI()) / 8;
+		EXPECT_TRUE(Tolerance<float>::isEqualLoosely(expected, actual));
+	}
+
 }
 
 TEST(SPHKernelTest, TestGetCubicSpilineByMatrix)
 {
 	SPHKernel<float> kernel;
-	Matrix3d<float> matrix;
-	const auto actual = kernel.getCubicSpline(Vector3d<float>(1, 0, 0), matrix);
-	const auto expected = 1.0f / (4 * Tolerance<float>::getPI());
-	EXPECT_TRUE(Tolerance<float>::isEqualLoosely(expected, actual));
+	{
+		Matrix3d<float> matrix;
+		const auto actual = kernel.getCubicSpline(Vector3d<float>(1, 0, 0), matrix);
+		const auto expected = 1.0f / (4 * Tolerance<float>::getPI());
+		EXPECT_TRUE(Tolerance<float>::isEqualLoosely(expected, actual));
+	}
+	{
+		Matrix3d<float> matrix = Vector3d<float>(0.5, 0.5, 0.5).toDiagonalMatrix();
+		const auto actual = kernel.getCubicSpline(Vector3d<float>(2, 0, 0), matrix);
+		const auto expected = 1.0f / (4 * Tolerance<float>::getPI()) / 8;
+		EXPECT_TRUE(Tolerance<float>::isEqualLoosely(expected, actual));
+	}
 }
