fa4c128e3d835811ccfdefe91689b18e532a8464
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2016-11-09 18:43:40 +0900

    PositionBasedを並列化．

--- Physics/PBSPHSolver.cpp
+++ Physics/PBSPHSolver.cpp
@@ -41,29 +41,42 @@ void PBSPHSolver::simulate(const float dt, const float effectRadius)
 		space.add(p);
 	}
 
-	for (auto p : particles) {
+	//for (auto p : particles) {
+#pragma omp parallel for
+	for(int i = 0; i < particles.size(); ++i) {
+		const auto p = particles[i];
 		const auto& neighbors = space.getNeighbor(p);
 		p->setNeighbors(neighbors);
 	}
 
 	for (int iter = 0; iter < 3; ++iter) {
-		for (auto p : particles) {
+		#pragma omp parallel for
+		for (int i = 0; i < particles.size(); ++i) {
+			const auto p = particles[i];
 			p->solveDensity();
 		}
 		//boundarySolver.solveDensity(particles);
-		for (auto p : particles) {
+#pragma omp parallel for
+		for (int i = 0; i < particles.size(); ++i) {
+			const auto p = particles[i];
 			p->solveConstrantGradient();
 		}
 		//boundarySolver.solveConstraintGradient(particles);
-		for (auto p : particles) {
+#pragma omp parallel for
+		for (int i = 0; i < particles.size(); ++i) {
+			const auto p = particles[i];
 			p->solveDensityConstraint();
 		}
 
-		for (auto p : particles) {
+#pragma omp parallel for
+		for (int i = 0; i < particles.size(); ++i) {
+			const auto p = particles[i];
 			p->solvePositionCorrection();
 		}
 		//boundarySolver.solveCorrectPosition(particles);
-		for (auto p : particles) {
+#pragma omp parallel for
+		for (int i = 0; i < particles.size(); ++i) {
+			const auto p = particles[i];
 			p->updatePredictPosition(dt);
 		}
 	} 
